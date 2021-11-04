b38f7aa93581cbd616454f4192691a2055028db6
MyUsername <my@email.com>
2021-11-04 14:44:02 +0100

    inizio

---
+++ script.js
@@ -0,0 +1,4 @@
+// 1. Creiamo il nostro array di oggetti che rappresentano ciascun membro del team
+// 2. Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// 3. facciamo in modo che quando si aggiungano gli input nella pagina appaia la card aggiuntiva
+
